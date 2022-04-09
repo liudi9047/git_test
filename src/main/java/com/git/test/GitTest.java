a66a3be5baefc2a97466e2c86ddfeb5d94d08b7a
nic <nic.leo@163.com>
2022-04-09 22:18:30 +0800

    hot-fix first commit

--- src/main/java/com/git/test/GitTest.java
+++ src/main/java/com/git/test/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello,world");
         System.out.println("hello,world2");
         System.out.println("hello hot-fix");
+        System.out.println("hello hot-fix2");
     }
 }

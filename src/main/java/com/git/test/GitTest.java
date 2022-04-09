db63d979f26e3e2971d837ee895d259720f65099
nic <nic.leo@163.com>
2022-04-09 21:46:21 +0800

    first commit

---
+++ src/main/java/com/git/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.git.test;
+
+/**
+ * @auther nic
+ * @create 2022-04-09-21:42
+ * @Description:
+ * @Parameters:
+ * @Return:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}

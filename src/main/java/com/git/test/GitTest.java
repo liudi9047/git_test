0917d628e509e126866652a949f9d9463eeb52b5
nic <nic.leo@163.com>
2022-04-09 21:55:15 +0800

    third commit

--- src/main/java/com/git/test/GitTest.java
+++ src/main/java/com/git/test/GitTest.java
@@ -1,14 +1,9 @@
 package com.git.test;
 
-/**
- * @auther nic
- * @create 2022-04-09-21:42
- * @Description:
- * @Parameters:
- * @Return:
- */
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,world2");
     }
 }
